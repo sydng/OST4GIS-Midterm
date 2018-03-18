6cb356ce857a9e54111c513a63f638aaa14bc399
sydng <35381098+sydng@users.noreply.github.com>
2018-03-18 18:41:58 -0400

    filtering functions

--- JS/midTerm.js
+++ JS/midTerm.js
@@ -12,7 +12,73 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
 }).addTo(map);
 
 //CALLING DATA
-var dataset;
-var promise = $.ajax("https://raw.githubusercontent.com/sydng/OST4GIS-Midterm/master/litter_index_lines.geojson")
-.then(function(res) { dataset = JSON.parse(res);})
-.then(function() {console.log(dataset);});
+var litterIndex;
+var majorArterials;
+//var recycleBank;
+
+var promise = "https://raw.githubusercontent.com/sydng/OST4GIS-Midterm/master/litter_index_lines.geojson";
+//var promise2 = "https://raw.githubusercontent.com/sydng/OST4GIS-Midterm/master/WasteBaskets_Big_Belly.geojson";
+//var promise3 = "https://raw.githubusercontent.com/sydng/OST4GIS-Midterm/master/recyclebank_participation.geojson";
+
+var myStyle = function(feature) {
+  switch(feature.properties.street_class) {
+    case 2: return {color: "red"};      //major arterial
+    case 3: return {color: "orange"};   //minor arterial
+    case 4: return {color: "yellow"};   //collector street
+    case 5: return {color: "purple"};   //local road
+    case 6: return {color: "blue"};
+    case 12: return {color: "white"};
+    case 13: return {color: "black"};
+    case 14: return {color: "pink"};
+    case 18: return {color: "green"};
+  }
+};
+
+var lowscoreFilter = function(feature) {
+  if(feature.properties.hundred_block_score <= 1 &&
+     feature.properties.street_class == 2) {
+          return feature;
+      }
+};
+
+var medscoreFilter = function(feature) {
+  if(feature.properties.hundred_block_score > 1.5 &&
+    feature.properties.hundred_block_score <= 3.5 &&
+    feature.properties.street_class == 2) {
+          return feature;
+      }
+};
+
+var highscoreFilter = function(feature) {
+  if(feature.properties.hundred_block_score > 3.5 && feature.properties.street_class == 2) {
+          return feature;
+      }
+};
+
+var stInterest = function(feature) {
+  if(feature.properties.street_class == 3) {
+    return feature;
+  }
+};
+
+var arterials = function(feature) {
+  if(feature.properties.street_class == 2) {
+    return feature;
+  }
+};
+
+$(document).ready(function() {
+  $.ajax(promise).done(function(data) {
+    var parsedData = JSON.parse(data);
+    majorArterials = L.geoJson(parsedData, {
+      style: myStyle,
+      filter: arterials
+    }).addTo(map);
+
+    litterIndex = L.geoJson(parsedData, {
+      color: "lightgray",
+      filter: stInterest
+    }).addTo(map);
+    //litterIndex.eachLayer(eachFeatureFunction);
+  });
+});
